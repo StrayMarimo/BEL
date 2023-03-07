ac738a2b738c838d75bbc1018e32d07311a71764
Nep <38284741+Re3ses@users.noreply.github.com>
2023-03-07 20:16:31 +0800

    added right camera boundary

--- BehindEnemyLines/Assets/Scripts/CameraMovement.cs
+++ BehindEnemyLines/Assets/Scripts/CameraMovement.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class CameraMovement : MonoBehaviour
 {
     public float speed = 2f;
     private Vector3 newPosition;
+    private float objectWidth;
     public GameObject Player;
     public GameObject startPoint;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
     }
 
     // Update is called once per frame
@@ -21,16 +24,31 @@ public class CameraMovement : MonoBehaviour
         newPosition.x = transform.position.x + Time.deltaTime * speed;
         transform.position = newPosition;
 
-      
         var lowerLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
-        var upperRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        var upperRight =
+            Camera
+                .main
+                .ScreenToWorldPoint(new Vector3(Screen.width,
+                    Screen.height,
+                    0));
 
-        if (Player.transform.position.x < lowerLeft.x || 
-            Player.transform.position.x > upperRight.x )
+        if (Player.transform.position.x < lowerLeft.x)
         {
             Player.transform.position = startPoint.transform.position;
             Camera.main.transform.position = startPoint.transform.position;
         }
+        else if (Player.transform.position.x > upperRight.x)
+        {
+            Player.transform.position =
+                new Vector3(upperRight.x,
+                    Player.transform.position.y,
+                    Player.transform.position.z);
+        }
 
+        transform.position =
+            new Vector3(Mathf
+                    .Clamp(transform.position.x, lowerLeft.x + objectWidth, upperRight.x - objectWidth),
+                transform.position.y,
+                transform.position.z);
     }
 }
