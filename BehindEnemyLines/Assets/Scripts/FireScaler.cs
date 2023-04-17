8280532cbe9fdc38dc552d2b3c65fe8bf6dec0fc
Nep <38284741+Re3ses@users.noreply.github.com>
2023-04-17 23:42:24 +0800

    changed wall of death to wall of fire. Added flame spawn and decay mechanic

---
+++ BehindEnemyLines/Assets/Scripts/FireScaler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class FireScaler : MonoBehaviour
+{
+    public bool isScaling = false; // Whether to enable scaling
+    public float scalingSpeed = 1.0f; // The speed at which to scale the sprite
+    public float targetSize = 2.0f; // The target size to scale the sprite to
+    public float startSize = 1.0f; // The starting size of the sprite
+    public float expiry = 10f;
+    public GameObject thisFire;
+    private Transform transformComponent; // Reference to the Transform component
+
+    void Start()
+    {
+        // Get a reference to the Transform component
+        transformComponent = GetComponent<Transform>();
+
+        // Set the starting scale of the transformComponent
+        transformComponent.localScale = new Vector3(startSize, startSize, 1f);
+
+        // Destroy fire game object after a certain duration
+        Destroy(thisFire, expiry);
+    }
+
+    void Update()
+    {
+        // If scaling is enabled, scale the sprite towards the target size
+        if (isScaling)
+        {
+            float newSize = Mathf.MoveTowards(transformComponent.localScale.x, targetSize, scalingSpeed * Time.deltaTime);
+            transformComponent.localScale = new Vector3(newSize, newSize, 1f);
+            Debug.Log("Current size: " + transformComponent.localScale);
+        }
+        // Otherwise, scale the sprite back to the starting size
+        else
+        {
+            float newSize = Mathf.MoveTowards(transformComponent.localScale.x, startSize, scalingSpeed * Time.deltaTime);
+            transformComponent.localScale = new Vector3(newSize, newSize, 1f);
+        }        
+    }
+}
--- BehindEnemyLines/Assets/Scripts/WallOfDeath.cs
+++ BehindEnemyLines/Assets/Scripts/WallOfDeath.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 
 public class WallOfDeath : MonoBehaviour
 {
-    public bool shouldMove;
-    public float speed = 2f;
-    public float wallOffset = -10f;
-    public Transform playerTransform;
-    public Transform startPointTransform;
-    public Collider2D wallCollider;
+    public bool shouldMove;  // If the wall should move
+    public float speed = 2f; // The speed that the wall moves
+    public float wallOffset = -10f; // The amount of offset that the wall has away from the player on reset
+    public GameObject gameObjPrefab; // The prefab of the game object to spawn
+    public float spawnOffset; // The offset at which to spawn the new game object
+    public float spawnInterval; // The interval at which to spawn the new game object
+    private float lastSpawnPos; // The last position at which the game object was spawned
+    private GameObject[] fires;
+    public Transform playerTransform; // Player
+    public Transform startPointTransform; // The starting point of the level
+    public Collider2D wallCollider; // The collider for the wall
 
     void Start()
     {
@@ -28,6 +33,8 @@ public class WallOfDeath : MonoBehaviour
         {
             wallCollider.isTrigger = true;
         }
+
+        lastSpawnPos = transform.position.x; // Initialize lastSpawnPos to the starting position of the wall
     }
 
     // On collision with the wall
@@ -47,9 +54,19 @@ public class WallOfDeath : MonoBehaviour
             transform.position = new Vector3
                 (
                     startPointTransform.position.x + wallOffset, 
-                    startPointTransform.position.y + .3f, 
+                    startPointTransform.position.y, 
                     startPointTransform.position.z
                 );
+
+            // Restart flame spawning process
+            lastSpawnPos = transform.position.x; // Initialize lastSpawnPos to the starting position of the wall
+
+            fires = GameObject.FindGameObjectsWithTag("Fire"); // Store all flame currently present in an array and delete them
+            // Destroy each fire game object 
+            foreach (GameObject flame in fires)
+            {
+                Destroy(flame);
+            }
         }
     }
 
@@ -62,6 +79,18 @@ public class WallOfDeath : MonoBehaviour
                     transform.position.y, 
                     transform.position.z
                 );
+
+            // Check if the wall has moved by an amount equal to the spawn interval
+            if (transform.position.x - lastSpawnPos >= spawnInterval)
+            {
+                // Spawn a new game object
+                // Vector3 spawnPos = transform.position + new Vector3(spawnOffset, transform.position.y, 0);
+                Vector3 spawnPos = new Vector3(transform.position.x + spawnOffset, transform.position.y + -4.6f, transform.position.z + 3);
+                Instantiate(gameObjPrefab, spawnPos, Quaternion.identity);
+                lastSpawnPos += spawnInterval;
+            }
+
+
         }
     }
 }
