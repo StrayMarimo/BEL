276d25511c658a9b45a48cdebd0de1d422f00025
Angel <107756227+StrayMarimo@users.noreply.github.com>
2023-04-18 12:34:31 +0800

    feat: added scoring

--- BehindEnemyLines/Assets/Scripts/PlayerMovement.cs
+++ BehindEnemyLines/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 // attached to Player
 // handles player movement and transition animations
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,11 +11,17 @@ public class PlayerMovement : MonoBehaviour
     public bool isInBulletZone;
     private int jumpableCount = 0;  // counter to keep track of how many jumpable surfaces are below the player
     private bool canJump {get { return jumpableCount > 0;}} // a property indicating whether the player can jump
-
+    private int score = 0;
+    private float distance = 0;
     private Rigidbody2D rb;
     private SpriteRenderer p_SpriteRenderer;
     private Animator p_Animator;
+    private GameObject ScoreCanvas;
     float time = 0.5f;
+
+    [SerializeField]
+    Text Score;
+    public int totalScore = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +29,8 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         p_Animator = GetComponent<Animator>();
         p_SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-
+        ScoreCanvas = GameObject.FindGameObjectWithTag("Score");
+        Score = ScoreCanvas.GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
@@ -43,7 +51,14 @@ public class PlayerMovement : MonoBehaviour
         } else if (Move > 0)
         {
             p_SpriteRenderer.flipX = false;
+            if (transform.position.x > distance){
+                distance = transform.position.x;
+                score += 1;
+            }
+               
         }
+        totalScore = score / 50;
+        Score.text = "Score: " + totalScore.ToString();
 
         // Animations
         if (rb.velocity.x == 0 || !canJump)
